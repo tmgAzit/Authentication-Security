cb3261befcdbef653ea20b502b10c97bbe66a504
tmgAzit <azit.ghising@gmail.com>
2022-01-26 13:15:27 +1100

    Add bcrypt salting on hashing authentication.

--- app.js
+++ app.js
@@ -5,7 +5,10 @@ const express = require('express');
 const ejs = require('ejs');
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
-const encrypt = require('mongoose-encryption');
+const bcrypt = require('bcrypt');
+const saltRounds = 10;
+// const md5 = require('md5'); //level 3 Hashing Authentication
+// const encrypt = require('mongoose-encryption'); ////for level 2 encryption
 
 const app = express();
 
@@ -16,11 +19,11 @@ const userSchema = new mongoose.Schema({
   password: String,
 });
 
-// level 2 & level 3(process.env) encryption
-userSchema.plugin(encrypt, {
-  secret: process.env.SECRET,
-  encryptedFields: ['password'],
-});
+// level 2 (mongoose-encryption, process.env) encryption
+// userSchema.plugin(encrypt, {
+//   secret: process.env.SECRET,
+//   encryptedFields: ['password'],
+// });
 
 const User = mongoose.model('User', userSchema);
 
@@ -47,32 +50,42 @@ app.get('/register', function (req, res) {
 });
 
 app.post('/register', function (req, res) {
-  const newUser = new User({
-    email: req.body.username,
-    password: req.body.password,
-  });
-  // level One authentication
-  newUser.save(function (err) {
-    if (err) {
-      console.log(err);
-    } else {
-      res.render('secrets');
-    }
+  bcrypt.hash(req.body.password, saltRounds, function (err, hash) {
+    const newUser = new User({
+      email: req.body.username,
+      // password: md5(req.body.password), ////level 3 hash authentication
+      password: hash,
+    });
+    // level 1 authentication
+    newUser.save(function (err) {
+      if (err) {
+        console.log(err);
+      } else {
+        res.render('secrets');
+      }
+    });
   });
 });
 
 app.post('/login', function (req, res) {
   const username = req.body.username;
+  // const password = md5(req.body.password); ///level 3 hashing md5 method
   const password = req.body.password;
-  // level one authentication
+
+  // level 1 authentication
   User.findOne({ email: username }, function (err, foundUser) {
     if (err) {
       console.log(err);
     } else {
       if (foundUser) {
-        if (foundUser.password === password) {
-          res.render('secrets');
-        }
+        // if (foundUser.password === password) {
+        //   res.render('secrets'); ////////////////level 1 authentication
+        // }
+        bcrypt.compare(password, foundUser.password, function (err, result) {
+          if (result === true) {
+            res.render('secrets');
+          }
+        });
       }
     }
   });
